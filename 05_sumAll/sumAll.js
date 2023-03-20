b6d2bea23b81e87dc36632cf8e7715497a9180ef
KKaged <dhigaredaleon707@gmail.com>
2023-03-20 21:39:17 +0900

    finished 04,05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (sum1, sum2) {
+  if (!Number.isInteger(sum1) || !Number.isInteger(sum2)) return `ERROR`;
+  if (sum1 < 0 || sum2 < 0) return `ERROR`;
+  if (sum1 > sum2) {
+    const temp = sum1;
+    sum1 = sum2;
+    sum2 = temp;
+  }
+  let sum = 0;
+  for (let i = sum1; i < sum2 + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
