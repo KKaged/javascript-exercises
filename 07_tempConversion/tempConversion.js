37929edb2d87aa22abdecc355e30de78005a7994
KKaged <dhigaredaleon707@gmail.com>
2023-03-24 21:25:06 +0900

    Finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num1) {
+  return Math.round(((num1 - 32) * 5) / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num2) {
+  return Math.round((num2 * 9) / 5 + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
