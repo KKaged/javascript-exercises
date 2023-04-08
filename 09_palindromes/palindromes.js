7a2bacae88f4616cbafed3535ef2b4fefce415e8
KKaged <dhigaredaleon707@gmail.com>
2023-04-08 14:46:28 +0900

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const str = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (string = string.split().reverse().join("") == str);
 };
 
 // Do not edit below this line
